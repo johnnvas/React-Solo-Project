ee957dc90db9d13763cc5da053941e50a59398c2
Johnn <j.christian.vasquez@gmail.com>
2021-06-17 16:15:12 -0500

    phase1

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+// backend/app.js
+const routes = require('./routes');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -21,16 +23,20 @@ if (!isProduction) {
 }
 // helmet helps set a variety of headers to better secure your app
 app.use(helmet({
-  contentSecurityPolicy: false
+    contentSecurityPolicy: false
 }));
 
 // Set the _csrf token and create req.csrfToken method
 app.use(
   csurf({
-    cookie: {
-      secure: isProduction,
-      sameSite: isProduction && "Lax",
-      httpOnly: true,
-    },
-  })
-);
+      cookie: {
+          secure: isProduction,
+          sameSite: isProduction && "Lax",
+          httpOnly: true,
+        },
+    })
+    );
+
+app.use(routes); // Connect all the routes
+
+module.exports = app;
