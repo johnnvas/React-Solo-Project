8a092808b9438b5f0c9de1777cab870fa4abd229
Johnn <j.christian.vasquez@gmail.com>
2021-06-18 01:50:30 -0500

    password validation!

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,11 +5,27 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
+
+
 const router = express.Router()
 
 // Log in
 router.post(
   '/',
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -53,6 +69,6 @@ router.get(
     } else return res.json({});
   }
 );
- 
+
 
 module.exports = router;
