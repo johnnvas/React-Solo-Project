980c390c6baa4e657c6de0504794c963d9509318
Johnn <j.christian.vasquez@gmail.com>
2021-06-18 12:02:50 -0500

    Signup Form part2

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import './SignupForm.css';
 import * as sessionActions from "../../store/session";
 
 function SignupFormPage() {
